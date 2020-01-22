9cb0858f64e08c5395bbcda3d4500a5061ac5c10
= <=>
2020-01-22 02:45:23 +0000

    Adds mouse click functionality to tabs in tabbed pane

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectDisplayWindow.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectDisplayWindow.java
@@ -9,6 +9,8 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ObjectDisplayWindow {
 
@@ -27,7 +29,6 @@ public class ObjectDisplayWindow {
 
         addTabbedPaneListener();
 
-
     }
 
     public JPanel getContentWindow() {
@@ -35,23 +36,29 @@ public class ObjectDisplayWindow {
     }
 
     public void addTabbedPaneListener(){
-        tabbedPane.addChangeListener(changeEvent -> {
-            //Get the title of the tab that was selected
-            int tabPosition = tabbedPane.getSelectedIndex();
-            String tabTitle = tabbedPane.getTitleAt(tabPosition);
+        tabbedPane.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
 
-            if(tabTitle.equals("+")){
-                //Open dialog window
+                //Get the position and title of the tab that was selected
+                int tabPosition = tabbedPane.getSelectedIndex();
+                String tabTitle = tabbedPane.getTitleAt(tabPosition);
 
-                //Set tab position -1,
-
-                //Add new tab
+                if (tabTitle.equals("+")) {
+                    //Add a new tab before the '+' tab
+                    addTab(tabPosition);
+                }
             }
-            else{
-
-            }
-
         });
 
+    }
+
+    public void addTab(int tabPosition){
+        //Remove '+' tab from the tabbed pane
+        tabbedPane.removeTabAt(tabPosition);
+        //Add new tab to pane
+        tabbedPane.addTab("Tab", new JPanel());
+        //Re-add '+' tab to end of tabbed pane
+        tabbedPane.addTab("+", new JPanel());
     }
 }
