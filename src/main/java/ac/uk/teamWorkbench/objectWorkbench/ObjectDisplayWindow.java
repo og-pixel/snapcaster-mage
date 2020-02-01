32c47e237a49576fea81bc55e3bb7066ed394966
Matthew Levene <matthew.levene@hotmail.co.uk>
2020-02-01 12:34:32 +0000

    Adds change listener to tabbed pane

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectDisplayWindow.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectDisplayWindow.java
@@ -4,6 +4,11 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ObjectDisplayWindow {
 
@@ -19,9 +24,34 @@ public class ObjectDisplayWindow {
     public ObjectDisplayWindow(Project project, ToolWindow toolWindow) {
         this.project = project;
         this.toolWindow = toolWindow;
+
+        addTabbedPaneListener();
+
+
     }
 
     public JPanel getContentWindow() {
         return contentWindow;
+    }
+
+    public void addTabbedPaneListener(){
+        tabbedPane.addChangeListener(changeEvent -> {
+            //Get the title of the tab that was selected
+            int tabPosition = tabbedPane.getSelectedIndex();
+            String tabTitle = tabbedPane.getTitleAt(tabPosition);
+
+            if(tabTitle.equals("+")){
+                //Open dialog window
+
+                //Set tab position -1,
+
+                //Add new tab
+            }
+            else{
+
+            }
+
+        });
+
     }
 }
