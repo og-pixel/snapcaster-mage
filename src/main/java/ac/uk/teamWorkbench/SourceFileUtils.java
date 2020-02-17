aa6fa83e15753500146948ea2a08bafb9a3e79ec
Kacper <u1674145@unimail.hud.ac.uk>
2020-02-17 13:30:26 +0000

    Testing SourceFileUtils to return project java files in PopupAction message tool.

--- src/main/java/ac/uk/teamWorkbench/SourceFileUtils.java
+++ src/main/java/ac/uk/teamWorkbench/SourceFileUtils.java
@@ -6,6 +6,7 @@ package ac.uk.teamWorkbench;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 
 import java.util.Collection;
 
@@ -14,7 +15,12 @@ public class SourceFileUtils {
 
     private static final String extension = "java";
 
-    public static Collection<VirtualFile> getAllFilesByExt(Project project){
+    public static Collection<VirtualFile> getAllFilesByExt(Project project) {
         return FilenameIndex.getAllFilesByExt(project, extension);
     }
+
+    public static Collection<VirtualFile> getAllFilesByExtInProjectScope(Project project) {
+        return FilenameIndex.getAllFilesByExt(project, extension, GlobalSearchScope.projectScope(project));
+    }
+
 }
