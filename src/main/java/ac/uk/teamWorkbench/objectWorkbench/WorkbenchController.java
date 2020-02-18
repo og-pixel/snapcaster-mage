347af1dd66a23821fc2da3ea93ab3bfe8e784739
Luke McCann <developer-lukemccann@outlook.com>
2020-02-18 10:04:38 +0000

    BugFix: fixes issue with warning text layout by spacing content

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/WorkbenchController.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/WorkbenchController.java
@@ -127,7 +127,7 @@ public class WorkbenchController {
         StringBuilder out = new StringBuilder();
         if(validator.hasProtectedCharacter(title) || validator.isProtectedCharacter(title)) {
             for(String item : protectedCharacters) {
-                out.append("       -").append(item).append("\n");
+                out.append("       - ").append("'"+ item + "'").append("\n");
             }
             dialogFactory.displayWarningDialog(GUI.getTabbedPane(),
                     "Please ensure there are not protected characters. \n" +
