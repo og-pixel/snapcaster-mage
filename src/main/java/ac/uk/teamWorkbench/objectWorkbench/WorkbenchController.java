d3e2f9276110e28467b951f42d8ddf5aa800d285
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2020-02-24 15:51:19 +0000

    Repaired objectCreationWindow, shows a window again.

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/WorkbenchController.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/WorkbenchController.java
@@ -103,7 +103,7 @@ public class WorkbenchController {
         removeTab(index);
         addTab();
         addNewTabButton();
-        new ObjectCreationWindow(true);
+        new ObjectCreationWindow(true).showAndGet();
     }
 
     /**
