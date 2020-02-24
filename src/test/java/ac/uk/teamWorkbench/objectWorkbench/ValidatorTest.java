49cf27382bbc45ab1f0a59854f254d2b471f509b
= <matthew.levene@hotmail.co.uk>
2020-02-24 20:10:25 +0000

    Adds TestValidator class to unit testing directory

---
+++ src/test/java/ac/uk/teamWorkbench/objectWorkbench/ValidatorTest.java
@@ -0,0 +1,20 @@
+package ac.uk.teamWorkbench.objectWorkbench;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorTest {
+
+    @Test
+    public void isValidAddRequest() {
+    }
+
+    @Test
+    public void isProtectedCharacter() {
+    }
+
+    @Test
+    public void hasProtectedCharacter() {
+    }
+}
