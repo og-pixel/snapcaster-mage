d5aabb3d7ea678e13fa53c98855f56a28f0260e7
= <matthew.levene@hotmail.co.uk>
2020-02-24 21:26:00 +0000

    Adds unit test cases to the TestValidator class

--- src/test/java/ac/uk/teamWorkbench/objectWorkbench/ValidatorTest.java
+++ src/test/java/ac/uk/teamWorkbench/objectWorkbench/ValidatorTest.java
@@ -2,19 +2,57 @@ package ac.uk.teamWorkbench.objectWorkbench;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ValidatorTest {
 
     @Test
-    public void isValidAddRequest() {
+    public void testIsValidAddRequest() {
+        //Initialise an empty ArrayList for testing
+        Validator validator = new Validator(new ArrayList<>());
+
+        //If the string equals '+' it should return true
+        assertTrue("Should return true", validator.isValidAddRequest("+"));
+        //If string is not equal to '+' it should return false
+        assertFalse("Should return false", validator.isValidAddRequest("Object"));
     }
 
     @Test
-    public void isProtectedCharacter() {
+    public void testIsProtectedCharacter() {
+        //Initialise an ArrayList containing protected characters
+        ArrayList<String> protectedChars = new ArrayList<>();
+        protectedChars.add("A");
+        protectedChars.add("*");
+        protectedChars.add("!");
+
+        //Initialise the validator object using the protected character array
+        Validator validator = new Validator(protectedChars);
+
+        assertTrue("Should return true if character is protected",
+                validator.isProtectedCharacter("*"));
+
+        assertFalse("Should return false if character is not protected",
+        validator.isProtectedCharacter("B"));
     }
 
     @Test
-    public void hasProtectedCharacter() {
+    public void testHasProtectedCharacter() {
+        //Initialise an ArrayList containing protected characters
+        ArrayList<String> protectedChars = new ArrayList<>();
+        protectedChars.add("A");
+        protectedChars.add("*");
+        protectedChars.add("!");
+
+        //Initialise the validator object using the protected character array
+        Validator validator = new Validator(protectedChars);
+
+        assertTrue("Should return true if protected is contained in string",
+                validator.hasProtectedCharacter("*Object*"));
+
+        assertFalse("Should return false if no protected characters are in the string",
+                validator.hasProtectedCharacter("Object"));
     }
 }
