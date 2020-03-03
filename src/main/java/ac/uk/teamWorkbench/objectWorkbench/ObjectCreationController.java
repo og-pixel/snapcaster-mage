9a00aa012bc402c16a6132b9a93992fbb2d4fb1e
Milosz Jakubanis <milosz.jakubanis@protonmail.com>
2020-03-03 16:27:20 +0000

    Adding Object Pool and moving some of the controller logic to it as this instance will be needed in multiple controllers and only instantiated once and later updated to save resources.

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectCreationController.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/ObjectCreationController.java
@@ -29,7 +29,8 @@ public class ObjectCreationController {
     private ObjectCreationWindow GUI;
 
     //HashMap of classes in the Project
-    private Map<String, ClassReflection> projectClassMap = new HashMap<>();
+//    private Map<String, ClassReflection> projectClassMap = new HashMap<>();
+
 
 
     /**
@@ -40,87 +41,87 @@ public class ObjectCreationController {
         this.GUI = GUI;
     }
 
-    Map<String, VirtualFile> findCompiledClasses(VirtualFile root) {
-        Map<String, VirtualFile> list = new HashMap<>();
-        return findCompiledClasses(root.getChildren(), list);
-    }
-
-    private Map<String, VirtualFile> findCompiledClasses(VirtualFile[] virtualFile, Map<String, VirtualFile> list) {
-        for (VirtualFile file : virtualFile) {
-            if (file.isDirectory()) {
-                findCompiledClasses(file.getChildren(), list);
-            } else {
-                list.put(file.getNameWithoutExtension(), file);
-            }
-        }
-        return list;
-    }
+//    Map<String, VirtualFile> findCompiledClasses(VirtualFile root) {
+//        Map<String, VirtualFile> list = new HashMap<>();
+//        return findCompiledClasses(root.getChildren(), list);
+//    }
+//
+//    private Map<String, VirtualFile> findCompiledClasses(VirtualFile[] virtualFile, Map<String, VirtualFile> list) {
+//        for (VirtualFile file : virtualFile) {
+//            if (file.isDirectory()) {
+//                findCompiledClasses(file.getChildren(), list);
+//            } else {
+//                list.put(file.getNameWithoutExtension(), file);
+//            }
+//        }
+//        return list;
+//    }
 
     /**
      * Finds all compiled classes from the root of the project and
      * populates the projectHashList
      * It does also needs to be a first project in; "Project Settings/Modules".
      */
-    void findProjectClasses() {
-        VirtualFile projectRoot;
-        try {
-            projectRoot = SourceFileUtils.getInstance().getCompilerModule().get(0);
-        } catch (ArrayIndexOutOfBoundsException indexException) {
-            System.out.println(indexException.getMessage());
-            return;
-        }
+//    void findProjectClasses() {
+//        VirtualFile projectRoot;
+//        try {
+//            projectRoot = SourceFileUtils.getInstance().getCompilerModule().get(0);
+//        } catch (ArrayIndexOutOfBoundsException indexException) {
+//            System.out.println(indexException.getMessage());
+//            return;
+//        }
+//
+//        File allFiles = new File(Objects.requireNonNull(projectRoot.getCanonicalPath()));
+//
+//        Map<String, VirtualFile> compiledClassesList;
+//        compiledClassesList = findCompiledClasses(projectRoot);
+//        //Load classes into class loader
+//        try {
+//            classLoader = URLClassLoader.newInstance(new URL[]{allFiles.toURI().toURL()});
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        //Loop over all compiled classes and extract methods and variables and save them as String
+//        // in ObjectReflection Class
+//        ClassReflection classReflection;
+//        Class<?> loadedClass;
+//        for (Map.Entry<String, VirtualFile> entry : compiledClassesList.entrySet()) {
+//            String className = entry.getValue().getNameWithoutExtension();
+//            loadedClass = loadClass(entry.getValue(), className);
+//            classReflection = new ClassReflection(className, loadedClass);
+//
+//            projectClassMap.put(className, classReflection);
+//            getClassMethods(loadedClass).forEach(methodName ->
+//                    projectClassMap.get(className).addMethod(methodName));
+//            getClassVariables(loadedClass).forEach(variableName ->
+//                    projectClassMap.get(className).addVariable(variableName));
+//            getClassConstructor(loadedClass).forEach(constructorName ->
+//                    projectClassMap.get(className).addConstructor(constructorName));
+//        }
+//    }
 
-        File allFiles = new File(Objects.requireNonNull(projectRoot.getCanonicalPath()));
-
-        Map<String, VirtualFile> compiledClassesList;
-        compiledClassesList = findCompiledClasses(projectRoot);
-        //Load classes into class loader
-        try {
-            classLoader = URLClassLoader.newInstance(new URL[]{allFiles.toURI().toURL()});
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-
-        //Loop over all compiled classes and extract methods and variables and save them as String
-        // in ObjectReflection Class
-        ClassReflection classReflection;
-        Class<?> loadedClass;
-        for (Map.Entry<String, VirtualFile> entry : compiledClassesList.entrySet()) {
-            String className = entry.getValue().getNameWithoutExtension();
-            loadedClass = loadClass(entry.getValue(), className);
-            classReflection = new ClassReflection(className, loadedClass);
-
-            projectClassMap.put(className, classReflection);
-            getClassMethods(loadedClass).forEach(methodName ->
-                    projectClassMap.get(className).addMethod(methodName));
-            getClassVariables(loadedClass).forEach(variableName ->
-                    projectClassMap.get(className).addVariable(variableName));
-            getClassConstructor(loadedClass).forEach(constructorName ->
-                    projectClassMap.get(className).addConstructor(constructorName));
-        }
-    }
-
-    Class<?> loadClass(VirtualFile virtualFile, String className) {
-        PsiManager psiManager = SourceFileUtils.getInstance().getPsiManager();
-        PsiFile psiFile;
-
-        Class<?> loadedClass = null;
-        String packageName;
-
-        try {
-            psiFile = psiManager.findFile(virtualFile);
-            packageName = ((PsiJavaFile) psiFile).getPackageName();
-            if (packageName.isEmpty()) {
-                loadedClass = classLoader.loadClass(className);
-            } else {
-                loadedClass = classLoader.loadClass(packageName + "." + className);
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return loadedClass;
-    }
+//    Class<?> loadClass(VirtualFile virtualFile, String className) {
+//        PsiManager psiManager = SourceFileUtils.getInstance().getPsiManager();
+//        PsiFile psiFile;
+//
+//        Class<?> loadedClass = null;
+//        String packageName;
+//
+//        try {
+//            psiFile = psiManager.findFile(virtualFile);
+//            packageName = ((PsiJavaFile) psiFile).getPackageName();
+//            if (packageName.isEmpty()) {
+//                loadedClass = classLoader.loadClass(className);
+//            } else {
+//                loadedClass = classLoader.loadClass(packageName + "." + className);
+//            }
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        return loadedClass;
+//    }
 
     /**
      * Add Event Listeners
@@ -211,49 +212,49 @@ public class ObjectCreationController {
     /**
      * Extract methods from the Class by performing reflection
      *
-     * @param loadedClass Class name that methods are extracted from.
+//     * @param loadedClass Class name that methods are extracted from.
      * @return List of methods belonging to that class.
      */
-    ArrayList<Method> getClassMethods(Class<?> loadedClass) {
-        Method[] methods = loadedClass.getDeclaredMethods();
-        return new ArrayList<>(Arrays.asList(methods));
-    }
+//    ArrayList<Method> getClassMethods(Class<?> loadedClass) {
+//        Method[] methods = loadedClass.getDeclaredMethods();
+//        return new ArrayList<>(Arrays.asList(methods));
+//    }
+//
+//    /**
+//     * Extract variables from the Class by performing reflection
+//     *
+//     * @param loadedClass Class name that variables are extracted from.
+//     * @return List of variables belonging to that class.
+//     */
+//    ArrayList<Field> getClassVariables(Class<?> loadedClass) {
+//        Field[] fields = loadedClass.getDeclaredFields();
+//        return new ArrayList<>(Arrays.asList(fields));
+//    }
+//
+//    /**
+//     * Extract available constructors from the Class by performing reflection
+//     *
+//     * @param loadedClass Class name that variables are extracted from.
+//     * @return List of constructors belonging to that class.
+//     */
+//    ArrayList<Constructor<?>> getClassConstructor(Class<?> loadedClass) {
+//        Constructor<?>[] constructors = loadedClass.getDeclaredConstructors();
+//        return new ArrayList<>(Arrays.asList(constructors));
+//    }
 
-    /**
-     * Extract variables from the Class by performing reflection
-     *
-     * @param loadedClass Class name that variables are extracted from.
-     * @return List of variables belonging to that class.
-     */
-    ArrayList<Field> getClassVariables(Class<?> loadedClass) {
-        Field[] fields = loadedClass.getDeclaredFields();
-        return new ArrayList<>(Arrays.asList(fields));
-    }
-
-    /**
-     * Extract available constructors from the Class by performing reflection
-     *
-     * @param loadedClass Class name that variables are extracted from.
-     * @return List of constructors belonging to that class.
-     */
-    ArrayList<Constructor<?>> getClassConstructor(Class<?> loadedClass) {
-        Constructor<?>[] constructors = loadedClass.getDeclaredConstructors();
-        return new ArrayList<>(Arrays.asList(constructors));
-    }
-
-    public Class<?> loadSelectedClass(String className) {
-        VirtualFile projectRoot = null;
-        try {
-            projectRoot = SourceFileUtils.getInstance().getCompilerModule().get(0);
-        } catch (ArrayIndexOutOfBoundsException indexException) {
-            System.out.println(indexException.getMessage());
-            System.exit(1);
-        }
-        Map<String, VirtualFile> compiledClassesList;
-        compiledClassesList = findCompiledClasses(projectRoot);
-
-        return loadClass(compiledClassesList.get(className), className);
-    }
+//    public Class<?> loadSelectedClass(String className) {
+//        VirtualFile projectRoot = null;
+//        try {
+//            projectRoot = SourceFileUtils.getInstance().getCompilerModule().get(0);
+//        } catch (ArrayIndexOutOfBoundsException indexException) {
+//            System.out.println(indexException.getMessage());
+//            System.exit(1);
+//        }
+//        Map<String, VirtualFile> compiledClassesList;
+//        compiledClassesList = findCompiledClasses(projectRoot);
+//
+//        return loadClass(compiledClassesList.get(className), className);
+//    }
 
     Map<String, ClassReflection> getProjectClassMap() {
         return projectClassMap;
