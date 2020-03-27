8a565ef3955d86b229f53f3d5976ad0530a669c8
= <matthew.levene@hotmail.co.uk>
2020-03-27 09:33:35 +0000

    Adds functionality to draw JLabels to the UI

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/objectDisplay/LeftPane.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/objectDisplay/LeftPane.java
@@ -1,4 +1,31 @@
 package ac.uk.teamWorkbench.objectWorkbench.objectDisplay;
 
-public class LeftPane {
+import javax.swing.*;
+import java.awt.*;
+
+public class LeftPane extends JPanel {
+
+    private GridBagConstraints cons = new GridBagConstraints();
+
+    public LeftPane(Object[] parameters, Class<?>[] paramTypes){
+        setLayout(new GridBagLayout());
+
+        cons.weighty = 0.1;
+        cons.weighty = 0.1;
+        cons.insets = new Insets(10,10 ,0,0);
+
+        drawLabels(parameters, paramTypes);
+    }
+    private void drawLabels(Object[] params, Class<?>[] paramTypes){
+        for(int i = 0; i < params.length; i++){
+            cons.gridx = 0;
+            cons.gridy = i;
+            add(new JLabel("Type: " + paramTypes[i] + "   "), cons);
+
+            cons.gridx = 1;
+            cons.gridy = i;
+            add(new JLabel("Value: " + params[i]), cons);
+
+        }
+    }
 }
