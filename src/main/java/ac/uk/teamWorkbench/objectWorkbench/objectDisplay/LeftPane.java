60a9180e4918739c3c0407cc22c133e4c1291194
= <matthew.levene@hotmail.co.uk>
2020-03-27 10:28:10 +0000

    Updates drawLabels method, so that the panel now uses GridBagLayout as the layout manager

--- src/main/java/ac/uk/teamWorkbench/objectWorkbench/objectDisplay/LeftPane.java
+++ src/main/java/ac/uk/teamWorkbench/objectWorkbench/objectDisplay/LeftPane.java
@@ -2,14 +2,18 @@ package ac.uk.teamWorkbench.objectWorkbench.objectDisplay;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class LeftPane extends JPanel {
 
-    private GridBagConstraints cons = new GridBagConstraints();
+    private GridBagConstraints cons;
     private static LeftPane instance;
+    private ArrayList<JPanel> panelArray;
 
     public LeftPane(){
-        setLayout(new GridBagLayout());
+
+        cons = new GridBagConstraints();
+        panelArray = new ArrayList<>();
 
         cons.weighty = 0.1;
         cons.weighty = 0.1;
@@ -20,15 +24,38 @@ public class LeftPane extends JPanel {
         if(instance == null) instance = new LeftPane();
         return instance;
     }
-    public void drawLabels(Object[] params, Class<?>[] paramTypes){
+
+    /**
+     * Draws JLabels to a JPanel and returns the JPanel
+     */
+    public JPanel drawLabels(Object[] params, Class<?>[] paramTypes){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
         for(int i = 0; i < params.length; i++){
             cons.gridx = 0;
             cons.gridy = i;
-            add(new JLabel("Type: " + paramTypes[i] + "   "), cons);
+            panel.add(new JLabel("Type: " + paramTypes[i] + "    "), cons);
 
             cons.gridx = 1;
             cons.gridy = i;
-            add(new JLabel("Value: " + params[i]), cons);
+            panel.add(new JLabel("Value: " + params[i]), cons);
         }
+
+        return panel;
+    }
+
+    /**
+     * Stores a JPanel to the JPanel ArrayList
+     */
+    public void addPanel(JPanel panel){
+        panelArray.add(panel);
+    }
+
+    /**
+     * Retrieves a JPanel from the JPanel ArrayList by index
+     */
+    public JPanel getPanel(int index){
+        return panelArray.get(index);
     }
 }
